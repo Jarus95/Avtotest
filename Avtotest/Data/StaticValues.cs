cb9e91eb04e56779feaed92827dba2965855b49d
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-09-16 16:53:16 +0500

    Add questions

---
+++ Avtotest/Data/StaticValues.cs
@@ -0,0 +1,10 @@
+﻿using Avtotest.Models.Json;
+
+namespace Avtotest.Data
+{
+    public static class StaticValues
+    {
+        public static List<QuestionsToJson> questions = questions = new List<QuestionsToJson>();
+        public static bool isLog;
+    }
+}
---
+++ Avtotest/Migrations/20230916092429_question.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Avtotest.Migrations
+{
+    /// <inheritdoc />
+    public partial class question : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Choice");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ChoiceId",
+                table: "Questions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChoiceId",
+                table: "Questions");
+
+            migrationBuilder.CreateTable(
+                name: "Choice",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Answer = table.Column<bool>(type: "bit", nullable: false),
+                    QuestionsId = table.Column<int>(type: "int", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Choice", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Choice_Questions_QuestionsId",
+                        column: x => x.QuestionsId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Choice_QuestionsId",
+                table: "Choice",
+                column: "QuestionsId");
+        }
+    }
+}
